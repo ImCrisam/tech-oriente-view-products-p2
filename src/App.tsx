8494e4edfa1de73a06e67322ca8b395e3dacf979
Mauricio Arias <cristian.arias@artemisa.tech>
2024-11-18 13:08:25 -0500

    get params url

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+interface QueryParams {
+  [key: string]: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryParams, setQueryParams] = useState<QueryParams>({});
+
+  useEffect(() => {
+    // Obtener parámetros de la URL usando URLSearchParams
+    const params = new URLSearchParams(window.location.search);
+    const paramsObject: QueryParams = {};
+
+    params.forEach((value, key) => {
+      paramsObject[key] = value;
+    });
+
+    setQueryParams(paramsObject);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Parámetros de la URL:</h1>
+      <pre>{JSON.stringify(queryParams, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App
